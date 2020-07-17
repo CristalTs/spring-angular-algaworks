4f00313376533d3d972b8f4662aa7645b4ff5aa8
CristalTs <cristaltshishimbi@yahoo.fr>
2020-07-17 00:46:47 -0300

    connection witj data base create categoria table

---
+++ src/main/resources/db/migration/V01__criar_e_registar_categorias.sql
@@ -0,0 +1,10 @@
+﻿create table public.categoria(
+	codigo serial primary key,
+	nome character varying(50) not null
+);
+
+insert into public.categoria(nome) values ('Lazer');
+insert into public.categoria(nome) values ('Alimentação');
+insert into public.categoria(nome) values ('Supermercado');
+insert into public.categoria(nome) values ('Farmácia');
+insert into public.categoria(nome) values ('Outros');
